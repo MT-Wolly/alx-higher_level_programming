394caa53c4c6b3a17c193d1383becee0578fcc51
Mustapha Wolly <wollymustapha@gmail.com>
2021-11-18 16:20:23 -0500

    function to print results of addition of all arguments after casting

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
+    length = len(sys.argv)
     result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    for arg in range(length):
+        if arg == 0:
+	    continue
+        result += int(sys.argv[i])
+    print("{}".format(result))
