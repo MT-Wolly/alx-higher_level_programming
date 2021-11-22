00285660c3a5f77a9e19f12939690ef5d6886e7e
Mustapha Wolly <wollymustapha@gmail.com>
2021-11-22 11:03:34 -0500

    function to print a string without characters c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for char in my_string:
-        if char not in 'Cc':
-            my_string = ''.join(char)
-    return my_string
+    new_string = [:]
+    for i in range(len(my_string)):
+        if (my_string[i] == 'c' or my_string[i] == 'C'):
+            continue
+        new_string = ''.join(my_string[i])
+    return new_string
