20f05231eb3f1c4a84f69b073929db9d73b31bd3
Mustapha Wolly <wollymustapha@gmail.com>
2021-11-22 10:45:40 -0500

    function to replace element in a copy list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
+
+"""Function to replace element at a particular index in a copy list"""
+
     if idx < 0:
         return (my_list)
-    if idx >= len(my_list) - 1:
+
+    elif idx >= len(my_list) - 1:
         return (my_list)
+
     else:
         new_list = my_list.copy()
         new_list[idx] = element
