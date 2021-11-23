9a3d82d3ada3b81914d1e3a65f87fbe44048d8ee
Mustapha Wolly <wollymustapha@gmail.com>
2021-11-22 21:26:17 -0500

    function to check if a number is divisible by two and append true or false

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    div = []
+    check_div = []
 
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            div.append(True)
+            check_div.append(True)
         else:
-            append(False)
+            check_div.append(False)
 
-    return (div)
+    return (check_div)
