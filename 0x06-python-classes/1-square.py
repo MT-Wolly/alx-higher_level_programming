dd77e5e711b1c2e6ad3c09e35ca9ea04fdc4b279
Mustapha Wolly <wollymustapha@gmail.com>
2021-12-07 18:21:26 -0500

    function to define a Class square by its size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defining a Square in a Square class"""
+
+
+class Square:
+    def __init__(self, size):
+        """Initialized method that stores size of a square
+
+        Args:
+            size (float): size of the square
+        """
+        self.__size = size
