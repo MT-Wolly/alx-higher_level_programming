04b32f5a8d51aaac99895abbb24a60a9b6b7ba26
Mustapha Wolly <wollymustapha@gmail.com>
2021-12-21 16:42:56 -0500

    function to append a file to the end of a text file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defining the function append_write"""
+
+
+def append_write(filename="", text=""):
+    """This function is used to write
+       the end of a filename the content of text.
+       Args:
+           filename (str): the file
+           text (str): the string to be appended
+       Returns:
+           the number of characters added
+    """
+    with open(filename, "a", encoding='utf-8') as f:
+        return f.write(text)
