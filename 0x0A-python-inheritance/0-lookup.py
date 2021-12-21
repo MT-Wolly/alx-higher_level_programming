2d7c8beb3aadac6c2092deaa1389a878c80fb674
Mustapha Wolly <wollymustapha@gmail.com>
2021-12-20 19:15:42 -0500

    function to lookup all attributes and objects in a directory

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-def lookup(obj):
-    """ Function that returns the list of available attributes
-        and methods of an object
-    Args:
-        obj: instance of the class
-    Returns:
-        List of attributes
-    """
+"""Function to return the list of available attributes and methods of an object"""
 
-    return dir(obj)
+
+def lookup(obj):
+    """This function returns all the list of available
+       attributes and methods of object"""
+    return list(dir(obj))
