c17b44dd22d437ffffea1c110face2a73f9abe65
Mustapha Wolly <wollymustapha@gmail.com>
2022-01-02 11:30:14 -0500

    updating the base class by adding a static method to return json representation of a list dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,8 +11,20 @@ class Base:
     
     def __init__(self, id=None):
         """This function initializes instances of the class"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ method to convert list to JSON string """
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+
+
+
