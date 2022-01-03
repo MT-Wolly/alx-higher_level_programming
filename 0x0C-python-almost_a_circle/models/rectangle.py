492f8e5018f08a11442167cdff93c35d48eea683
Mustapha Wolly <wollymustapha@gmail.com>
2022-01-03 02:43:10 -0500

    pep8 correction for rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,14 +5,13 @@ from models.base import Base
 
 class Rectangle(Base):
     """Defining a rectangle class with private instance attributes."""
-    def __init__(self, width, height, x =0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes instances of the class Rectangle"""
         self.width = width
         self.height = height
         self.x = x
         self.y = y
         super().__init__(id)
-    
 
     @property
     def width(self):
@@ -22,7 +21,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Width Setter"""
-        if type(value) !=  int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -42,7 +41,6 @@ class Rectangle(Base):
             raise ValueError("height must be > 0")
         self.__height = value
 
-
     @property
     def x(self):
         """x setter"""
@@ -56,7 +54,6 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
-
 
     @property
     def y(self):
@@ -72,11 +69,9 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-
     def area(self):
         """ function that returns the area of the rectangle object """
         return self.width * self.height
-    
 
     def display(self):
         """ Function that prints in stdout the rectangle instance with the character # """
@@ -86,7 +81,6 @@ class Rectangle(Base):
             rectangle = rectangle + ("#" * self.width) + "\n"
         
         print(rectangle, end='')
-
 
     def __str__(self):
         """ Function to define the string representation """
@@ -102,14 +96,13 @@ class Rectangle(Base):
         Function to update the public method by assigning 
         arguments and key/value arguments to attributes
         """
-        if args != None and len(args) != 0:
+        if args is not None and len(args) != 0:
             list_attr = ['id', 'width', 'height', 'x', 'y']
             for i in range(len(args)):
                 setattr(self, list_attr[i], args[i])
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-
 
     def to_dictionary(self):
         """ Method that returns a dictionary representation of a rectangle """
