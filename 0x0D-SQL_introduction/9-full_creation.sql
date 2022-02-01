8bd07325bab5fc1b5fa54d08e4692f43f60db852
Mustapha Wolly <wollymustapha@gmail.com>
2022-02-01 15:07:09 -0500

    query to create full table and insert values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,6 +2,7 @@
 -- query to create secon_table in database
 CREATE TABLE IF NOT EXISTS second_table (
 id INT,
+name VARCHAR(256),
 score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
