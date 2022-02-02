e83bc6fa07b0bd469511b0d53777394ae628d116
Mustapha Wolly <wollymustapha@gmail.com>
2022-02-02 17:56:09 -0500

    query cript that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- lists all privileges of the MySQL users
+-- query to list all privileges (GRANT) of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
